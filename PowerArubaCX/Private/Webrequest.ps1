8ba78cf229ee6ff18a3f96e190134545534767e0
Alexis La Goutte <alexis.lagoutte@gmail.com>
2018-07-10 09:08:06 +0200

    WebRequest: Add Invoke-ArubaCXWebRequest to ask ArubaCX switch
    For example to get list of all vlans : Invoke-ArubaCXWebRequest -url 'rest/v1/system/bridge/vlans' -method 'get'

---
+++ PowerArubaCX/Private/Webrequest.ps1
@@ -0,0 +1,61 @@
+#
+# Copyright 2018, Alexis La Goutte <alexis.lagoutte at gmail dot com>
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+function Invoke-ArubaCXWebRequest(){
+
+    Param(
+        [Parameter(Mandatory = $true)]
+        [String]$url,
+        [Parameter(Mandatory = $true)]
+        #Valid POST, GET...
+        [String]$method,
+        [Parameter(Mandatory = $false)]
+        [psobject]$body,
+        [Parameter(Mandatory = $false)]
+        [Microsoft.PowerShell.Commands.WebRequestSession]$sessionvariable
+    )
+
+    Begin {
+    }
+
+    Process {
+
+        $Server = ${DefaultArubaCXConnection}.Server
+        $fullurl = "https://${Server}/${url}"
+
+
+        if( -Not $PsBoundParameters.ContainsKey('sessionvariable') ){
+            $sessionvariable = $DefaultArubaCXConnection.session
+        }
+
+        try {
+            if($body){
+                $response = Invoke-WebRequest $fullurl -Method $method -body ($body | ConvertTo-Json) -Websession $sessionvariable
+            } else {
+                $response = Invoke-WebRequest $fullurl -Method $method -Websession $sessionvariable
+            }
+        }
+        catch {
+            If ($_.Exception.Response) {
+
+                $result = $_.Exception.Response.GetResponseStream()
+                $reader = New-Object System.IO.StreamReader($result)
+                $responseBody = $reader.ReadToEnd()
+                $responseJson =  $responseBody | ConvertFrom-Json
+
+                Write-Host "The Switch API sends an error message:" -foreground yellow
+                Write-Host
+                Write-Host "Error description (code): $($_.Exception.Response.StatusDescription) ($($_.Exception.Response.StatusCode.Value__))" -foreground yellow
+                Write-Host "Error details: $($responseJson.message)" -foreground yellow
+                Write-Host
+            }
+            throw "Unable to use switch API"
+        }
+        $response
+
+    }
+
+}
