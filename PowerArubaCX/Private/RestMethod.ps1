aa1cc2446f19c18e89aa64161859234012c22b37
Alexis La Goutte <alexis.lagoutte@gmail.com>
2019-02-26 17:08:12 +0100

    RestMethod: Add .SYNOPSIS/.DESCRIPTION/.EXAMPLE to Invoke-ArubaCXRestMethod

--- PowerArubaCX/Private/RestMethod.ps1
+++ PowerArubaCX/Private/RestMethod.ps1
@@ -6,6 +6,29 @@
 
 function Invoke-ArubaCXRestMethod {
 
+    <#
+      .SYNOPSIS
+      Invoke RestMethod with ArubaCX connection (internal) variable
+
+      .DESCRIPTION
+       Invoke RestMethod with ArubaCX connection variable (token, csrf..)
+
+      .EXAMPLE
+      Invoke-ArubaCXRestMethod -method "get" -uri "rest/v1/system"
+
+      Invoke-RestMethod with ArubaCX connection for get rest/v1/system
+
+      .EXAMPLE
+      Invoke-ArubaCXRestMethod "rest/v1/system"
+
+      Invoke-RestMethod with ArubaCX connection for get rest/v1/system uri with default GET method parameter
+
+      .EXAMPLE
+      Invoke-ArubaCXRestMethod -method "post" -uri "rest/v1/system" -body $body
+
+      Invoke-RestMethod with ArubaCX connection for post api/v2/cmdb/firewall/address uri with $body payload
+    #>
+
     Param(
         [Parameter(Mandatory = $true, position = 1)]
         [String]$uri,
