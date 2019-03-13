e671b3cc0365575e71a466f70c1cb059182c70be
Alexis La Goutte <alexis.lagoutte@gmail.com>
2019-03-13 14:01:54 +0100

    Interfaces: Add Get-ArubaCXInterfaces cmdlet
    for get ALL interfaces settings (lag, port, vlan...)
    No yet filter or extra option (like depth)...

---
+++ PowerArubaCX/Public/interfaces.ps1
@@ -0,0 +1,25 @@
+﻿#
+# Copyright 2019, Alexis La Goutte <alexis dot lagoutte at gmail dot com>
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+function Get-ArubaCXinterfaces {
+
+    <#
+      .SYNOPSIS
+      Get list of all Aruba CX Interfacess
+
+      .DESCRIPTION
+      Get list of all Aruba CX Interfaces (port, lag, vlan... with name, IP Address, description)
+
+      .EXAMPLE
+      Get-ArubaCXIntefarces
+
+      Get list of all interface (lag/port/vlan)
+
+    #>
+
+
+    $response = Invoke-ArubaCXRestMethod -uri 'rest/v1/system/interfaces' -method 'GET'
+    $response
+}
