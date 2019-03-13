7389ad35c908670eca3decf74dd617bf752d9bad
Alexis La Goutte <alexis.lagoutte@gmail.com>
2019-03-13 14:05:00 +0100

    ports: Add Get-ArubaCXPorts cmdlet
    for get ALL ports ettings (port, vlan, statistics...)
    No yet filter or extra option (like depth)...

---
+++ PowerArubaCX/Public/ports.ps1
@@ -0,0 +1,25 @@
+﻿#
+# Copyright 2019, Alexis La Goutte <alexis dot lagoutte at gmail dot com>
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+function Get-ArubaCXPorts {
+
+    <#
+      .SYNOPSIS
+      Get list of all Aruba CX Ports
+
+      .DESCRIPTION
+      Get list of all Aruba CX Ports (name, Mode, vlan, description....)
+
+      .EXAMPLE
+      Get-ArubaCXPorts
+
+      Get list of all ports
+
+    #>
+
+
+    $response = Invoke-ArubaCXRestMethod -uri 'rest/v1/system/ports' -method 'GET'
+    $response
+}
