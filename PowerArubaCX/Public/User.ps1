ceb0866f894c8b7b896e6d861828090eccfe726d
JM2K69 <bezettorres.jerome@neuf.fr>
2020-01-28 10:15:38 +0100

    Add Get-ArubaCX[Vlan|User|Firmware] cmdlets
    for get Vlan, User and Firmware

---
+++ PowerArubaCX/Public/User.ps1
@@ -0,0 +1,33 @@
+﻿function Get-ArubaCXUser {
+
+    <#
+      .SYNOPSIS
+      Get Aruba CX user
+
+      .DESCRIPTION
+      Get all informations about Aruba CX Users
+
+      .EXAMPLE
+      Get-ArubaCXusers
+
+      List all users in ArubaCX
+
+    #>
+    Param(
+    )
+
+    Begin {
+    }
+
+    Process {
+
+        $uri = "system/users"
+
+
+        $response = Invoke-ArubaCXRestMethod -uri $uri -method 'GET'
+        $response
+    }
+
+    End {
+    }
+}
---
+++ PowerArubaCX/Public/vlan.ps1
@@ -0,0 +1,52 @@
+﻿function Get-ArubaCXvlan {
+
+    <#
+      .SYNOPSIS
+      Get list of all Aruba CX Vlan
+
+      .DESCRIPTION
+      Get list of all Aruba CX Vlan (port, lag, vlan... with name, IP Address, description)
+
+      .EXAMPLE
+      Get-ArubaCXvlan
+      
+        Get list of all vlan, lag,with name IP and more
+
+    #>
+    Param(
+        [Parameter(Mandatory = $false)]
+        [ValidateRange(0, 3)]
+        [Int]$depth,
+        [Parameter(Mandatory = $false)]
+        [ValidateSet("configuration", "status", "statistics")]
+        [String]$selector,
+        [Parameter(Mandatory = $false)]
+        [String[]]$attributes
+    )
+
+    Begin {
+    }
+
+    Process {
+
+        $invokeParams = @{ }
+        if ( $PsBoundParameters.ContainsKey('depth') ) {
+            $invokeParams.add( 'depth', $depth )
+        }
+        if ( $PsBoundParameters.ContainsKey('selector') ) {
+            $invokeParams.add( 'selector', $selector )
+        }
+        if ( $PsBoundParameters.ContainsKey('attributes') ) {
+            $invokeParams.add( 'attributes', $attributes )
+        }
+
+        $uri = "system/vlans"
+
+
+        $response = Invoke-ArubaCXRestMethod -uri $uri -method 'GET' @invokeParams
+        $response
+    }
+
+    End {
+    }
+}
