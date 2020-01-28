183a40844b84ff29cbd8ff4deb99d6223d84454c
JM2K69 <bezettorres.jerome@neuf.fr>
2020-01-28 10:15:38 +0100

    Add Get-ArubaCX[Vlan|User|Firmware] cmdlets
    for get Vlan, User and Firmware

---
+++ PowerArubaCX/Public/Firmware.ps1
@@ -0,0 +1,32 @@
+﻿function Get-ArubaCXfirmware {
+
+    <#
+      .SYNOPSIS
+      Get Aruba CX firmware
+
+      .DESCRIPTION
+      Get all informations about Aruba CX firmware
+
+      .EXAMPLE
+      Get-ArubaCXfirmware
+
+            Get all informations about Aruba CX firmware, first image an secondary image
+    #>
+    Param(
+    )
+
+    Begin {
+    }
+
+    Process {
+
+        $uri = "firmware"
+
+
+        $response = Invoke-ArubaCXRestMethod -uri $uri -method 'GET'
+        $response
+    }
+
+    End {
+    }
+}
