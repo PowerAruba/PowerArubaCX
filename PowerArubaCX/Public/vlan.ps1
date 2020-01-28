5f41d405ee7332389027716f259cf766c5eac8ad
Alexis La Goutte <alexis.lagoutte@gmail.com>
2020-01-28 08:41:35 +0100

    Vlan: Add Add-ArubaCXVlan for Add Vlan...

--- PowerArubaCX/Public/vlan.ps1
+++ PowerArubaCX/Public/vlan.ps1
@@ -4,6 +4,73 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
+
+function Add-ArubaCXVlan {
+
+    <#
+        .SYNOPSIS
+        Add Aruba CX Vlan
+
+        .DESCRIPTION
+        Add Vlan (name, description, vsx_sync...)
+
+        .EXAMPLE
+        Add-ArubaCXVlan -name Vlan 2 -id 2
+
+        Add Vlan id 2 named Vlan 2
+    #>
+    Param(
+        [Parameter (Mandatory = $true)]
+        [ValidateRange(1, 4096)]
+        [int]$id,
+        [Parameter (Mandatory = $false)]
+        [string]$name,
+        [Parameter (Mandatory = $false)]
+        [string]$description,
+        [Parameter (Mandatory = $false)]
+        [string]$type,
+        [Parameter (Mandatory = $false)]
+        [string]$admin,
+        [Parameter (Mandatory = $False)]
+        [ValidateNotNullOrEmpty()]
+        [PSObject]$connection = $DefaultArubaSWConnection
+    )
+
+    Begin {
+    }
+
+    Process {
+
+        $uri = "system/vlans"
+
+        $_vlan = new-Object -TypeName PSObject
+
+        $_vlan | add-member -name "id" -membertype NoteProperty -Value $id
+
+        if ( $PsBoundParameters.ContainsKey('name') ) {
+            $_vlan | add-member -name "name" -membertype NoteProperty -Value $name
+        }
+
+        if ( $PsBoundParameters.ContainsKey('description') ) {
+            $_vlan | add-member -name "description" -membertype NoteProperty -Value $description
+        }
+
+        if ( $PsBoundParameters.ContainsKey('type') ) {
+            $_vlan | add-member -name "type" -membertype NoteProperty -Value $type
+        }
+
+        if ( $PsBoundParameters.ContainsKey('admin') ) {
+            $_vlan | add-member -name "admin" -membertype NoteProperty -Value $admin
+        }
+
+        $response = Invoke-ArubaCXRestMethod -uri $uri -method 'POST' -body $_vlan -connection $connection
+        $response
+    }
+
+    End {
+    }
+}
+
 function Get-ArubaCXVlan {
 
     <#
