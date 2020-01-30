3a350d9b789066aaae851055f614c209d11713c8
Alexis La Goutte <alexis.lagoutte@gmail.com>
2020-01-30 17:01:36 +0100

    Firmware: fix indent and add Multi Connection / VSX Peer

--- PowerArubaCX/Public/Firmware.ps1
+++ PowerArubaCX/Public/Firmware.ps1
@@ -1,18 +1,24 @@
-﻿function Get-ArubaCXfirmware {
+﻿function Get-ArubaCXFirmware {
 
     <#
-      .SYNOPSIS
-      Get Aruba CX firmware
+        .SYNOPSIS
+        Get Aruba CX firmware
 
-      .DESCRIPTION
-      Get all informations about Aruba CX firmware
+        .DESCRIPTION
+        Get all informations about Aruba CX firmware
 
-      .EXAMPLE
-      Get-ArubaCXfirmware
+        .EXAMPLE
+        Get-ArubaCXfirmware
 
-            Get all informations about Aruba CX firmware, first image an secondary image
+        Get all informations about Aruba CX firmware, first image an secondary image
     #>
+
     Param(
+        [Parameter(Mandatory = $false)]
+        [switch]$vsx_peer,
+        [Parameter (Mandatory = $False)]
+        [ValidateNotNullOrEmpty()]
+        [PSObject]$connection = $DefaultArubaCXConnection
     )
 
     Begin {
@@ -20,10 +26,14 @@
 
     Process {
 
+        $invokeParams = @{ }
+        if ( $PsBoundParameters.ContainsKey('vsx_peer') ) {
+            $invokeParams.add( 'vsx_peer', $true )
+        }
+
         $uri = "firmware"
 
-
-        $response = Invoke-ArubaCXRestMethod -uri $uri -method 'GET'
+        $response = Invoke-ArubaCXRestMethod -uri $uri -method 'GET' -connection $connection @invokeParams
         $response
     }
 
