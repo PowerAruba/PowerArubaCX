be7680033637dde65e9b8bf40ddc1c36237fed30
Alexis La Goutte <alexis.lagoutte@gmail.com>
2020-04-02 07:15:03 +0200

    StaticRoutes : Add Get-ArubaCXStaticRoutes
    for get Static Routes Information (address_family, prefix, static_nexthops, type)

---
+++ PowerArubaCX/Public/StaticRoutes.ps1
@@ -0,0 +1,89 @@
+#
+# Copyright 2020, Alexis La Goutte <alexis dot lagoutte at gmail dot com>
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+function Get-ArubaCXStaticRoutes {
+
+    <#
+        .SYNOPSIS
+        Get list of all Aruba CX Route
+
+        .DESCRIPTION
+        Get list of all Aruba CX Static Rout (address_family, prefix, static_nexthops, type)
+
+        .EXAMPLE
+        Get-ArubaCXStaticRoutes
+
+        Get list of all Static Route information (address_family, prefix, static_nexthops, type)
+
+        .EXAMPLE
+        Get-ArubaCXStaticRoutes -prefix 192.0.2.0/24
+
+        Get Static Route information of prefix 192.0.2.0/24
+    #>
+
+    [CmdletBinding(DefaultParametersetname = "Default")]
+    Param(
+        [Parameter (Mandatory = $false, ParameterSetName = "prefix", position = "1")]
+        [string]$prefix,
+        [Parameter(Mandatory = $false)]
+        [ValidateRange(1, 4)]
+        [Int]$depth,
+        [Parameter(Mandatory = $false)]
+        [ValidateSet("configuration", "status", "statistics", "writable")]
+        [String]$selector,
+        [Parameter(Mandatory = $false)]
+        [String[]]$attributes,
+        [Parameter(Mandatory = $false)]
+        [string]$vrf = "default",
+        [Parameter(Mandatory = $false)]
+        [switch]$vsx_peer,
+        [Parameter (Mandatory = $False)]
+        [ValidateNotNullOrEmpty()]
+        [PSObject]$connection = $DefaultArubaCXConnection
+    )
+
+    Begin {
+    }
+
+    Process {
+
+
+        $invokeParams = @{ }
+        if ( $PsBoundParameters.ContainsKey('depth') ) {
+            $invokeParams.add( 'depth', $depth )
+        }
+        if ( $PsBoundParameters.ContainsKey('selector') ) {
+            $invokeParams.add( 'selector', $selector )
+        }
+        if ( $PsBoundParameters.ContainsKey('attributes') ) {
+            $invokeParams.add( 'attributes', $attributes )
+        }
+        if ( $PsBoundParameters.ContainsKey('vsx_peer') ) {
+            $invokeParams.add( 'vsx_peer', $true )
+        }
+
+        $uri = "system/vrfs/$vrf/static_routes"
+
+        # you can directly filter by prefix
+        if ( $PsBoundParameters.ContainsKey('prefix') ) {
+            #replace / by %2F
+            $prefix = $prefix -replace '/', '%2F'
+            $uri += "/$prefix"
+        }
+
+        $response = Invoke-ArubaCXRestMethod -uri $uri -method 'GET' -connection $connection @invokeParams
+
+        #Add id parameter when use writable type selector
+        if ( $PsBoundParameters.ContainsKey('selector') -and $PsBoundParameters.ContainsKey('prefix') -and $selector -eq "writable" ) {
+            $response | add-member -name "prefix" -membertype NoteProperty -Value $prefix
+        }
+
+        $response
+    }
+
+    End {
+    }
+}
