42a733a2c79fd959c771ccb2b5e420489f4f0146
Alexis La Goutte <alexis.lagoutte@gmail.com>
2020-04-12 19:59:21 +0200

    StaticRoutes(Tests): Add Tests for Add/Get/Remove Static Routes
    Need to enhance vrf part (for add and remove)

--- Tests/common.ps1
+++ Tests/common.ps1
@@ -10,6 +10,9 @@ $script:pester_vlan = 85 #vlan id for Vlan test
 $script:pester_vlan2 = 86 #vlan id for Vlan test (for affect a second vlan to interface)
 $script:pester_interface = "1/1/1" #interface id for test...
 $script:pester_vrf = "pester_vrf" #interface id for test...
+$script:pester_sr_ip4 = "192.0.2.0" # Network for Static Route
+$script:pester_sr_mask = "24" # Netmask for Static Route
+$script:pester_sr = $pester_sr_ip4 + "/" + $pester_sr_mask
 
 . ../credential.ps1
 #TODO: Add check if no ipaddress/login/password info...
---
+++ Tests/integration/StaticRoutes.ps1
@@ -0,0 +1,188 @@
+#
+# Copyright 2020, Alexis La Goutte <alexis dot lagoutte at gmail dot com>
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+. ../common.ps1
+
+Describe "Get Static Route" {
+    BeforeALL {
+        #Add a blackhole static route on vrf default
+        Add-ArubaCXStaticRoutes -address_family ipv4 -prefix_ip4 $pester_sr_ip4 -prefix_ip4_mask $pester_sr_mask -type blackhole
+    }
+
+    It "Get Static Route Does not throw an error" {
+        {
+            Get-ArubaCXStaticRoutes
+        } | Should Not Throw
+    }
+
+    It "Get ALL Static Route" {
+        $sr = Get-ArubaCXStaticRoutes
+        $sr.count | Should -Not -Be $NULL
+    }
+
+    It "Get Static Route ($pester_sr)" {
+        $sr = Get-ArubaCXStaticRoutes -prefix $pester_sr
+        $sr.prefix | Should -Be $pester_sr
+    }
+
+    It "Get Static Route ($pester_sr) and confirm (via Confirm-ArubaCXStaticRoutes)" {
+        $sr = Get-ArubaCXStaticRoutes -prefix $pester_sr
+        Confirm-ArubaCXStaticRoutes ($sr) | Should -Be $true
+    }
+
+    #Get with attribute, depth...
+    Context "Selector" {
+
+        It "Get Static Route with selector equal configuration" {
+            {
+                Get-ArubaCXStaticRoutes -selector configuration
+            } | Should Not Throw
+        }
+
+        It "Get Static Route with selector equal statistics" {
+            {
+                Get-ArubaCXStaticRoutes -selector statistics
+            } | Should Not Throw
+        }
+
+        It "Get Static Route with selector equal status" {
+            {
+                Get-ArubaCXStaticRoutes -selector status
+            } | Should Not Throw
+        }
+
+        It "Get Static Route with selector equal writable" {
+            {
+                Get-ArubaCXStaticRoutes -selector writable
+            } | Should Not Throw
+        }
+    }
+
+    Context "Depth" {
+
+        It "Get Static Route with depth equal 1" {
+            {
+                Get-ArubaCXStaticRoutes -depth 1
+            } | Should Not Throw
+        }
+
+        It "Get Static Route with depth equal 2" {
+            {
+                Get-ArubaCXStaticRoutes -depth 2
+            } | Should Not Throw
+        }
+
+        It "Get Static Route with depth equal 3" {
+            {
+                Get-ArubaCXStaticRoutes -depth 3
+            } | Should Not Throw
+        }
+
+        It "Get Static Route with depth equal 4" {
+            {
+                Get-ArubaCXStaticRoutes -depth 4
+            } | Should Not Throw
+        }
+    }
+
+    Context "Attribute" {
+
+        It "Get Static Route with one attribute (prefix)" {
+            $sr = Get-ArubaCXStaticRoutes -prefix $pester_sr -attribute prefix
+            @($sr).count | Should -be 1
+            $sr.prefix | Should -Not -BeNullOrEmpty
+        }
+
+        It "Get Static Route with two attributes (prefix, type)" {
+            $sr = Get-ArubaCXStaticRoutes -prefix $pester_sr -attribute prefix, type
+            @($sr).count | Should -be 1
+            $sr.prefix | Should -Not -BeNullOrEmpty
+            $sr.type | Should -Not -BeNullOrEmpty
+        }
+
+    }
+
+    Context "Search" {
+        It "Search Static Route by name ($pester_sr)" {
+            $sr = Get-ArubaCXStaticRoutes -prefix $pester_sr
+            @($sr).count | Should -be 1
+            $sr.prefix | Should -Be $pester_sr
+        }
+
+    }
+
+    AfterAll {
+        Get-ArubaCXStaticRoutes -prefix $pester_sr | Remove-ArubaCXStaticRoutes -vrf default -confirm:$false
+        #Reverse CheckPoint ?
+    }
+}
+
+Describe "Add Static Route" {
+
+    AfterEach {
+        Get-ArubaCXStaticRoutes -prefix $pester_sr | Remove-ArubaCXStaticRoutes -vrf default -confirm:$false
+        #Reverse CheckPoint ?
+    }
+
+    It "Add Static Route $pester_sr (type forward)" {
+        Add-ArubaCXStaticRoutes -address_family ipv4 -prefix_ip4 $pester_sr_ip4 -prefix_ip4_mask $pester_sr_mask -type forward
+        $sr = Get-ArubaCXStaticRoutes -prefix $pester_sr
+        $sr.prefix | Should -Be $pester_sr
+        $sr.address_family | Should -Be "ipv4"
+        $sr.type | Should -Be "forward"
+        #$sr.vrf | Should -Be "default"
+    }
+
+    It "Add Static Route $pester_sr (type reject)" {
+        Add-ArubaCXStaticRoutes -address_family ipv4 -prefix_ip4 $pester_sr_ip4 -prefix_ip4_mask $pester_sr_mask -type reject
+        $sr = Get-ArubaCXStaticRoutes -prefix $pester_sr
+        $sr.prefix | Should -Be $pester_sr
+        $sr.address_family | Should -Be "ipv4"
+        $sr.type | Should -Be "reject"
+        #$sr.vrf | Should -Be "default"
+    }
+
+    It "Add Static Route $pester_sr (type blackhole)" {
+        Add-ArubaCXStaticRoutes -address_family ipv4 -prefix_ip4 $pester_sr_ip4 -prefix_ip4_mask $pester_sr_mask -type blackhole
+        $sr = Get-ArubaCXStaticRoutes -prefix $pester_sr
+        $sr.prefix | Should -Be $pester_sr
+        $sr.address_family | Should -Be "ipv4"
+        $sr.type | Should -Be "blackhole"
+        #$sr.vrf | Should -Be "default"
+    }
+
+    #It "Add Static Route $pester_sr (type forward on $pester_vrf VRF)" {
+    #    Add-ArubaCXStaticRoutes -address_family ipv4 -prefix_ip4 $pester_sr_ip4 -prefix_ip4_mask $pester_sr_mask -type forward -vrf $pester_vrf
+    #    $sr = Get-ArubaCXStaticRoutes -prefix $pester_sr
+    #    $sr.prefix | Should -Be $pester_sr
+    #    $sr.address_family | Should -Be "ipv4"
+    #    $sr.type | Should -Be "blackhole"
+    #    $sr.vrf | Should -Be $pester_vrf
+    #}
+}
+
+Describe "Remove Static Route" {
+
+    BeforeEach {
+        #Always add Static Route $pester_sr...
+        Add-ArubaCXStaticRoutes -address_family ipv4 -prefix_ip4 $pester_sr_ip4 -prefix_ip4_mask $pester_sr_mask -type blackhole
+    }
+
+    It "Remove Static Route $pester_sr by prefix" {
+        Remove-ArubaCXStaticRoutes -prefix $pester_sr -vrf default -confirm:$false
+        $sr = Get-ArubaCXStaticRoutes
+        $sr.$pester_sr | Should -Be $NULL
+    }
+
+    It "Remove Static Route $pester_sr by pipeline" {
+        $sr = Get-ArubaCXStaticRoutes -prefix $pester_sr
+        $sr | Remove-ArubaCXStaticRoutes -vrf default -confirm:$false
+        $sr = Get-ArubaCXStaticRoutes
+        $sr.$pester_sr | Should -Be $NULL
+    }
+
+}
+
+Disconnect-ArubaCX -confirm:$false
